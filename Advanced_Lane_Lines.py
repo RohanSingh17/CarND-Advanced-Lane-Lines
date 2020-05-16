952ec3756ff41fe3228688402f95fd2a51d8dd72
Rohan <dudes8686@gmail.com>
2020-05-15 22:31:30 -0400

    Rectangle on road V2

--- Advanced_Lane_Lines.py
+++ Advanced_Lane_Lines.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from moviepy.editor import VideoFileClip
 import cv2
 import glob
 
 def print_images(orig,mod,nm):
-    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 12))
+    f, ((ax1, ax2)) = plt.subplots(1, 2, figsize=(24, 12))
     f.tight_layout()
 
     if len(orig.shape)==3:
@@ -99,7 +100,7 @@ def sobel_threshold(img, orient='m',kern=3, thresh=(30, 100),prn=0,nm='sobel_thr
 
     return binary_image
 
-def perspective_transform(img,src,dst,inv=0,prn=0,nm='perspective_transform'):
+def perspective_transform(img,src,dst,inv=0,rect=0,prn=0,nm='perspective_transform'):
     orig=np.copy(img)
 
     if len(img.shape)==3:
@@ -108,18 +109,29 @@ def perspective_transform(img,src,dst,inv=0,prn=0,nm='perspective_transform'):
         shp=img.shape[::-1]
 
     if inv==1:
-        M = cv2.getPerspectiveTransform(dst, src)
-    else:
-        M=cv2.getPerspectiveTransform(src,dst)
+        temp=src
+        src=dst
+        dst=temp
 
+    M=cv2.getPerspectiveTransform(src,dst)
     warped=cv2.warpPerspective(img,M,shp,flags=cv2.INTER_LINEAR)
 
-    tupl=map(tuple,dst.astype(int))
-    rect_cord=tuple(tupl)
+    if rect==1:
+        out_img=np.dstack((warped,warped,warped))
+        in_img=np.dstack((img,img,img))
 
-    #warped = cv2.rectangle(warped, rect_cord[1], rect_cord[3], color=(0, 0, 255),thickness=5)
+        tupl=map(tuple,src.astype(int))
+        rect_cord1=tuple(tupl)
+        cv2.line(in_img, rect_cord1[0], rect_cord1[1], color=(0, 0, 255), thickness=5)
+        cv2.line(in_img, rect_cord1[2], rect_cord1[3], color=(0, 0, 255), thickness=5)
+
+        tupl=map(tuple,dst.astype(int))
+        rect_cord=tuple(tupl)
+        cv2.rectangle(out_img, rect_cord[1], rect_cord[3], color=(0, 0, 255),thickness=5)
+
+    #print_images(in_img, out_img, nm)
     if prn==1:
-        print_images(orig,warped,nm)
+        print_images(in_img,out_img,nm)
 
     return warped
 
@@ -153,7 +165,7 @@ def find_lanes(img,prn=0,nm='findlanes'):
         y_high=img.shape[0]-(i+1)*win_height
 
         leftx_low=left_curr-margin
-        leftx_high = left_curr + margin
+        leftx_high = left_curr +margin
         rightx_low=right_curr-margin
         rightx_high=right_curr+margin
 
@@ -187,10 +199,10 @@ def find_lanes(img,prn=0,nm='findlanes'):
     return leftx,lefty,rightx,righty
 
 def rad_curvature(warped,leftx,lefty,rightx,righty):
-    ym_per_pix=30/720
-    xm_per_pix=3.7/700
+    ym_per_pix=15/720
+    xm_per_pix=3.7/900
 
-    y_eval=warped.shape[0]*ym_per_pix
+    y_eval=(warped.shape[0])*ym_per_pix
 
     left_poly=np.polyfit(lefty*ym_per_pix,leftx*xm_per_pix,2)
     lp=np.poly1d(left_poly)
@@ -203,6 +215,9 @@ def rad_curvature(warped,leftx,lefty,rightx,righty):
     rad_left=np.abs(np.power((1+np.square(2*left_poly[0]*y_eval+left_poly[1])),1.5)/(2*left_poly[0]))
     rad_right = np.abs(np.power((1 + np.square(2 * right_poly[0] * y_eval + right_poly[1])), 1.5) / (2 * right_poly[0]))
 
+    left.radius_of_curvature=rad_left
+    right.radius_of_curvature=rad_right
+
     lane_center=(left_center+right_center)/2
     car_ln_center=np.abs((xm_per_pix*warped.shape[1]/2)-lane_center)
 
@@ -210,7 +225,7 @@ def rad_curvature(warped,leftx,lefty,rightx,righty):
 
 def fit_poly(warped,leftx,lefty,rightx,righty,prn=0,nm='fit_poly'):
     out_img=np.dstack((warped,warped,warped))*255
-    margin=100
+    margin=20
 
     left_poly=np.polyfit(lefty,leftx,2)
     right_poly=np.polyfit(righty,rightx,2)
@@ -232,12 +247,14 @@ def fit_poly(warped,leftx,lefty,rightx,righty,prn=0,nm='fit_poly'):
     pts_right=np.array([rightx_plot - margin, ploty,rightx_plot + margin, ploty]).T
     pts_right=pts_right.reshape(-1,2)
 
-    cv2.fillPoly(window_img,np.int64([pts_left]),color=(0,255,0))
-    cv2.fillPoly(window_img,np.int64([pts_right]), color=(0, 255, 0))
-    result=img_weighted(out_img,window_img)
+    poly = np.array([leftx_plot, ploty,rightx_plot, ploty]).T
+    poly = poly.reshape(-1, 2)
 
-    # plt.plot(leftx_plot,ploty,color='yellow')
-    # plt.plot(rightx_plot, ploty, color='yellow')
+    cv2.fillPoly(window_img,np.int64([pts_left]),color=(0,0,255))
+    cv2.fillPoly(window_img,np.int64([pts_right]), color=(255, 0, 0))
+    cv2.fillPoly(window_img, np.int64([poly]), color=(0, 255, 0))
+
+    result=img_weighted(out_img,window_img)
 
     if prn==1:
         print_images(out_img,result,nm)
@@ -250,39 +267,69 @@ def img_weighted(main_img,overlay_img,a=1,b=0.3):
 def image_text(img,string,coord=(100,100),type=4,scl=2,color=(255,255,255),thickness=2):
     return cv2.putText(img, string,coord, type, scl, (255, 255, 255), thickness)
 
+# Define a class to receive the characteristics of each line detection
+class Line():
+    def __init__(self):
+        # was the line detected in the last iteration?
+        self.detected = False
+        # x values of the last n fits of the line
+        self.recent_xfitted = []
+        #average x values of the fitted line over the last n iterations
+        self.bestx = None
+        #polynomial coefficients averaged over the last n iterations
+        self.best_fit = None
+        #polynomial coefficients for the most recent fit
+        self.current_fit = [np.array([False])]
+        #radius of curvature of the line in some units
+        self.radius_of_curvature = None
+        #distance in meters of vehicle center from the line
+        self.line_base_pos = None
+        #difference in fit coefficients between last and new fits
+        self.diffs = np.array([0,0,0], dtype='float')
+        #x values for detected line pixels
+        self.allx = None
+        #y values for detected line pixels
+        self.ally = None
 
+global i
+i=0
 
+left=Line()
+right=Line()
 
 cal_images='camera_cal/calibration*.jpg'
 dist_img=cv2.imread('camera_cal/calibration1.jpg')
+video1='harder_challenge_video.mp4'
 ret, mtx, dist, rvecs, tvecs=calibrate_camera(cal_images)
 
-src=np.float32([[190,718],[580,457],[705,457],[1140,719]])
-dest=np.float32([[200,710],[200,50],[1100,50],[1100,710]])
+src=np.float32([[190,720],[580,457],[705,457],[1140,720]])
+dest=np.float32([[200,720],[200,50],[1080,50],[1080,720]])
 
+#def img_proc(image):
 for name in glob.glob('test_images/*.jpg'):
-    lane_image=cv2.imread(name)
-    undst=cal_undistort(lane_image,mtx,dist)
+    image=cv2.imread(name)
+    i=i+1
+    undst=cal_undistort(image,mtx,dist)
     sobx=sobel_threshold(undst, orient='x',kern=3, thresh=(30, 250))
-    #soby=sobel_threshold(lane_image, orient='y',kern=3, thresh=(30, 250),prn=1)
-    #sobm=sobel_threshold(lane_image, orient='m',kern=3, thresh=(100, 250),prn=1)
-    #sobgr=sobel_threshold(lane_image, orient='gr',kern=3, thresh=(0.7, 1.1),prn=1)
-
+    #soby=sobel_threshold(undst, orient='y',kern=3, thresh=(30, 250))
+    # sobm=sobel_threshold(undst, orient='m',kern=3, thresh=(70, 250))
+    # sobgr=sobel_threshold(undst, orient='gr',kern=3, thresh=(0.7, 1.1))
     #color_threshold(lane_image,channel=0,thresh=(100,250),prn=1)
     col_binary=color_threshold(undst,channel=2,thresh=(100,250))
 
     combined=np.zeros_like(col_binary)
     combined[(sobx>0) | (col_binary>0)]=1
+    # print_images(sobx,soby,sobm,sobgr,col_binary,combined, nm='transform')
     #color_threshold(lane_image,channel=2,thresh=(30,250),prn=1)
     #print_images(lane_image,combined,nm='combined')
 
     warped=perspective_transform(combined,src,dest)
     leftx,lefty,rightx,righty=find_lanes(warped)
-    rad_left,rad_right,car_ln_center=rad_curvature(warped,leftx,lefty,rightx,righty)
     result,window_img,left_poly,right_poly,ploty=fit_poly(warped,leftx,lefty,rightx,righty)
+    rad_left,rad_right,car_ln_center=rad_curvature(warped,leftx,lefty,rightx,righty)
 
     warped=perspective_transform(window_img,src,dest,inv=1)
-    processed_img=img_weighted(lane_image,warped.astype(np.uint8))
+    processed_img=img_weighted(image,warped.astype(np.uint8))
 
     rad_str='Radius of Curvature = '+str(int((rad_left+rad_right)/2))+'m'
 
@@ -294,10 +341,16 @@ for name in glob.glob('test_images/*.jpg'):
     final_img=image_text(processed_img,rad_str,coord=(warped.shape[1]//2-500,50))
     final_img=image_text(processed_img,cen_str,coord=(warped.shape[1]//2-500,125))
 
-    output_filepath='output_images/'+name[12:]
-    cv2.imwrite(output_filepath,final_img)
+    # output_filepath='output_videos/project_video'+str(i)+'.jpg'
+    # cv2.imwrite(output_filepath,final_img[:,:,::-1])
+    #
+    print_images(image,final_img,nm='lanes')
+    print(left.radius_of_curvature,right.radius_of_curvature,sep='\t')
+    # return final_img
 
-    print_images(lane_image,final_img,nm='lanes')
-    #perspective_transform(dist_img)
+# output_filepath='output_videos/harder_challenge_video.mp4'
+# clip1=VideoFileClip(video1)
+# mod=clip1.fl_image(img_proc)
+# mod.write_videofile(output_filepath, audio=False)
 
 
